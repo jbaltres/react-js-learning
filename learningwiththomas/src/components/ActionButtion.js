731a196535c81db62249069dd042ab4e0aecf7a5
Jürgen Baltres <jbaltres@Jurgens-MBP.fritz.box>
2022-02-08 15:25:13 +0100

    added styled components

--- learningwiththomas/src/components/ActionButtion.js
+++ learningwiththomas/src/components/ActionButtion.js
@@ -1,23 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 
-/*const Button = styled.button`
-  color: white;
-  border: none;
-  height: 40px;
-  width: 40px;
-  text-align: center;
-  border-radius: 50px;
-  background-color: ${props =>
-    props.close ? "rgb(37, 206, 198)" : "rgb(255, 58, 80)"};
-  margin: 20px 0px 20px;
-  font-size: 20px;
-`;*/
+
 
 function ActionButton({currywurst}) {
+
+const Gundula = styled.button`
+  color: black;
+  border: none;
+  text-align: center;
+  font-size: 20px;
+  margin-top: 10px;
+`;
+
   return (
-    <button  onClick={(currywurst)}>
+    <Gundula onClick={(currywurst)}>
       Action!
-    </button>
+    </Gundula>
   );
 }
 
